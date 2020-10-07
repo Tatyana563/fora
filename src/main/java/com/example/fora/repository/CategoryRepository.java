1de0ab75286dd57587f97516988d39647795d4f5
Tanya <tatyana.petrenko@dstarlab.com>
2020-10-07 12:35:51 +0300

    pagination issue

---
+++ src/main/java/com/example/fora/repository/CategoryRepository.java
@@ -0,0 +1,19 @@
+package com.example.fora.repository;
+
+
+import com.example.fora.model.Category;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    boolean existsByUrl(String url);
+    @Query("from Category as c ORDER BY c.id ASC")
+    List<Category> getChunk(PageRequest pageable);
+}
